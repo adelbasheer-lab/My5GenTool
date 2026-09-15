6e961df69bb01305338049dda4bc21af28f36e89
adelbasheer-lab <adel.basheer@gmail.com>
2026-09-15 04:05:21 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '5-de-oro-v5';
+const CACHE_NAME = '5-de-oro-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
